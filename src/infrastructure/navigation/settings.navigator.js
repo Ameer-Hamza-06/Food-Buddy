00f25661ccbb2cce80cf6c72a2c85511792ed7ec
Ameer-Hamza-06 <63589166+Ameer-Hamza-06@users.noreply.github.com>
2022-05-02 01:46:56 +0500

    Mobile Camera Module
    Mobile camera for profile picture

--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 import { SettingsScreen } from '../../features/settings/screens/settings.screen';
 import { FavouritesScreen } from '../../features/settings/screens/favourites.screen';
-
+import { CameraScreen } from '../../features/settings/screens/camera.screen';
 const SettingsStack = createStackNavigator();
 
 export const SettingsNavigator = ({ route, navigation }) => {
@@ -21,6 +21,7 @@ export const SettingsNavigator = ({ route, navigation }) => {
         component={SettingsScreen}
       />
       <SettingsStack.Screen name="Favourites" component={FavouritesScreen} />
+      <SettingsStack.Screen name="Camera" component={CameraScreen} />
     </SettingsStack.Navigator>
   );
 };
